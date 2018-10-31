bae005a0c5780cd2e399b683a64180369f07ebbb
iSecFE <r3qu3stt1me0ut@gmail.com>
2018-10-31 13:51:49 +0800

    Update proxy.config.js

--- webpack/proxy.config.js
+++ webpack/proxy.config.js
@@ -10,7 +10,15 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      "/api": {
+          target: "http://backEndAddress:port",
+          changeOrigin: true,
+          pathRewrite: {
+              "^/api": "/backEndPath"
+          }
+      },
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
